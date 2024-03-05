9c9ba299f3d95a34e0530c3a86cc7c98df998935
Leonardo Naddei <naddeileonardo@gmail.com>
2024-03-05 21:36:48 +0100

    Generato html

--- generator.py
+++ generator.py
@@ -4,20 +4,23 @@ from loader import get_db_corsi
 
 environment = Environment(loader=FileSystemLoader("templates/"))
 template = environment.get_template("intervento.txt")
-templateCorso = environment.get_template("corso.txt")
+templateCorso = environment.get_template("corso.html")
 
 def genera_intervento(intervento):
     filename = "test"
-    with open(filename, "w") as file:
+    with open(filename, "w", "utf-8") as file:
         file.write(template.render(intervento.to_dict()))
     x = 10
     pass
+
 def genera_corso(corso):
-    filename = "test"
+    filename = "test.html"
     with open(filename, "w") as file:
         file.write(templateCorso.render(corso.to_dict()))
+
 def main():
     db_corsi = get_db_corsi()
     genera_corso(db_corsi[3])
+
 if __name__ == '__main__':
     main()
